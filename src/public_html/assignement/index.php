7655d7c4f33d8345f90d89614021f4762bbef283
Boris Janjetovic <bjanjetovic@mpunkt.com>
2026-06-19 21:42:06 +0200

    index.php review done!

--- src/public_html/assignement/index.php
+++ src/public_html/assignement/index.php
@@ -1,6 +1,7 @@
 <?php
+// @Fixme - Use strict types!!!
 
-class RatenKredit
+class RatenKredit // @Fixme - Could be also final class ? You dont need to extend it anymore
 {
     public function __construct() {
         ini_set('display_errors', 'off'); // @Fixme No way - you can not put ini_set in constructor
@@ -9,13 +10,14 @@ class RatenKredit
     {
         if (!$providers) {
             $providers = ['ing-diba','Smava',  'ba_fin']; // @Fixme - This needs to be defned as private variables or in newer PHP like Enums...
-        } else $providers = [$providers]; // @Fixme No comment :) .......... needs to bi inside brackets
+        } else $providers = [$providers]; // @Fixme No comment :) .......... at least needs to bi inside brackets
         $r = array(); // @Fixme - use []
 
         $ingdiba = "https://api.jsonbin.io/v3/b/65a6e50e266cfc3fde79aa14?meta=false&amount=$_GET[amount]"; // @Fixme - Uhhh .. Place to private variable, define in constuctor, $_GET not secure, it needs escaping, what about amount ???
         for ($i = 0; $i <= count($providers); $i++) { // @Fixme Its deprecated way of itteration
-            switch ($providers[$i]) {
+            switch ($providers[$i]) { // @Fixme - Switch is ok, but fom PHP version 8.0 use match
                 case 'ing-diba':
+                    // @Fixme - Whole part is badly written, not readable, unclear
                     $offer = file_get_contents($ingdiba, false, stream_context_create([
                         "http" => [
                             "method" => "GET",
@@ -25,8 +27,9 @@ class RatenKredit
                     $offer = json_decode($offer, true);
                     break;
                 case 'Smava':
+                    // @Fixme - Also needs refactorind for a better readability, throw out the commented code that is not used
                     $curl = curl_init();
-                    curl_setopt_array($curl, array(
+                    curl_setopt_array($curl, array( // @Fixme - up is stream_context... here cURL .. Where dissapeared consistency?
                         CURLOPT_RETURNTRANSFER => 1,
                         CURLOPT_URL => 'https://api.jsonbin.io/v3/b/65a6e71e1f5677401f1ebd2c?meta=false',
                         /*post does not work with mock server CURLOPT_POST => 1,
@@ -44,6 +47,7 @@ class RatenKredit
                     break;
                     case 'ba_fin':
                     // no api docs yet?
+                        // @Fixme - Where is default case ??? Where is break??
             }
             $r[$providers[$i]] = $offer;
         }
@@ -51,9 +55,9 @@ class RatenKredit
     }
 }
 
-if (@$_GET['submit']) {
+if (@$_GET['submit']) { // @Fixme - Do this never again!!! Silence opereator not allowed. Use validations!!!
     $ratenkredit = new RatenKredit();
-    $offers = $ratenkredit->get($_ENV['providers']);
+    $offers = $ratenkredit->get($_ENV['providers']); // @Fixme - Ok... how.. the hell came $_ENV here??
 }
 
 include(dirname(__FILE__).'/view.phtml');
